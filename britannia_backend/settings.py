fa7f7478426ef08d6c7f11469e903076c6230c01
Mahmoud Nasser <mahmoudnasser@Mahmouds-MacBook-Air.local>
2025-01-27 12:41:16 +0200

    upload static files

--- britannia_backend/settings.py
+++ britannia_backend/settings.py
@@ -124,15 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Add leading slash
+STATIC_ROOT = BASE_DIR / 'static'  # Serve static files from this directory
+STATICFILES_DIRS = []  # Empty in production (only used in development)
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'  # URL prefix for media files
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory for uploaded media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
